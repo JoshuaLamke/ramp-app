1619fa75dcba73f1db7efaf5ddb53aad9aba15a0
Joshua Lamke <joshua.lamke@gmail.com>
2021-03-16 22:31:06 -0400

    added data to app

--- src/reducers/alertsReducer.js
+++ src/reducers/alertsReducer.js
@@ -1,7 +1,11 @@
 let initialState = [
     {title:'Marginal PICA Model Value', date:'03/10/2021 at 23:26:10', isThreat: false},
     {title:'Artifact Insertion Detected', date:'03/10/2021 at 22:45:31', isThreat: true},
-    {title: 'Extraneous 3rd Party IP Check',date:'03/10/2021 at 20:05:08',isThreat: true}
+    {title: 'Extraneous 3rd Party IP Check',date:'03/10/2021 at 20:05:08',isThreat: true},
+    {title: 'Incoming Material Compromise',date:'02/7/2021 at 12:31:23',isThreat: true},
+    {title: 'Compromise Of MES - Manufacturing Engineering',date:'02/01/21 at 10:45:58',isThreat: true},
+    {title: 'Compromise Of Data Prep Software',date:'01/21/2021 at 21:05:47',isThreat: true},
+    {title: 'Compromise Of Mask Quality',date:'01/13/2021 at 19:07:13',isThreat: true},
 ]
 const alertsReducer = (state = initialState, action) => {
     switch(action.type) {
